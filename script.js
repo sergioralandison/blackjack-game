a5f2712a3b64e2ba6b08fd84609c2d261ac72a1b
sergioralandison <sergioralandison@outlook.com>
2021-12-06 15:17:54 +0100

    first step app finish

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-let firstCard = 13
+// The cards of the player
+let firstCard = 10
 let secondCard = 11
+
+// The result of the Blackjack, is still alive and have blacjack or not ?
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let gameMessage = ""
 
 if (sum <= 20) {
-    alert("Do you want to draw a new card ?")
+    gameMessage = "Do you want to draw a new card ?"
 } else if (sum === 21) {
-    alert("Wohoo! You've got Blackjack!")
+    gameMessage = "Wohoo! You've got Blackjack!"
     hasBlackJack = true
 } else {
-    alert("You're out of the game!")
+    gameMessage = "You're out of the game!"
     isAlive = false
 }
 
+console.log(gameMessage)
