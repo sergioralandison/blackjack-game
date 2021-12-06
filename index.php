f4582b390c10a2a355379009f642519b972f9ecf
sergioralandison <sergioralandison@outlook.com>
2021-12-06 15:47:10 +0100

    some tags structure added, link to style file

--- index.php
+++ index.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Blackjack Game</title>
 </head>
 
 <body>
-
-
+    <h1>Blackjack Game</h1>
+    <p id="message-el">Want to play a round ?</p>
+    <p>Cards:</p>
+    <p>Sum:</p>
+    <button id="start-el">Start Game</button>
     <script src="script.js"></script>
 </body>
 </html>
