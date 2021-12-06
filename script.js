429e2855576cbb841b09c954cde172404fe47dec
sergioralandison <sergioralandison@outlook.com>
2021-12-06 16:14:43 +0100

    Improve code, comments and names variables

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-// The cards of the player
+// Cards of the player
 let firstCard = 10
 let secondCard = 11
 
-// The result of the Blackjack, is still alive and have blacjack or not ?
-let sum = firstCard + secondCard
+// Blackjack seed variables : Booleans and Sum of cards
+const sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-let gameMessage = ""
 
-if (sum <= 20) {
-    gameMessage = "Do you want to draw a new card ?"
-} else if (sum === 21) {
-    gameMessage = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    gameMessage = "You're out of the game!"
-    isAlive = false
+// Displaying messages and results
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+// There, the function to render the game
+function renderGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card ?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+    sumEl.textContent = `Sum: ${sum}`
+    cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`
 }
+
