3ce95c245de5e78c12b3a1e8c04b6316ddd54755
sergioralandison <sergioralandison@outlook.com>
2021-12-06 16:44:48 +0100

    new card button added

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Blackjack Game</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Blackjack Game</title>
+    </head>
 
-<body>
-    <h1>Blackjack Game</h1>
-    <p id="message-el">Want to play a round ?</p>
-    <p id="cards-el">Cards:</p>
-    <p id="sum-el">Sum:</p>
-    <button id="start-el" onclick="renderGame()">Start Game</button>
-    <script src="script.js"></script>
-</body>
+    <body>
+        <h1>Blackjack Game</h1>
+        <p id="message-el">Want to play a round ?</p>
+        <p id="cards-el">Cards:</p>
+        <p id="sum-el">Sum:</p>
+        <button id="start-el" onclick="startGame()">Start game</button>
+        <button onclick="newCard()">New card</button>
+        <script src="script.js"></script>
+    </body>
 </html>
